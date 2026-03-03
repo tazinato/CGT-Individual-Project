d2649c9172f0ee5a11ac925e3e6ff5d4a26697fc
tazinato <141641942+tazinato@users.noreply.github.com>
2026-03-03 15:48:28 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import {
 import Header from "./components/Header";
 import About from "./components/About";
 import PokemonCard from "./components/PokemonCard";
-import { pokemonData } from './Data/pokemonData.jsx';
+import { pokemonData } from './data/pokemonData.jsx';
 
 const AppContext = createContext();
 
